083cadfc575a3b346560f534c9fb6069da7d2173
insung jung <hunter10@naver.com>
2018-03-24 21:56:16 +0900

    local first commit

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic; 		//Allows us to use Lists.
+using Random = UnityEngine.Random; 		//Tells Random to use the Unity Engine random number generator.
+
+	
+public class BoardManager : MonoBehaviour
+{
+	// Using Serializable allows us to embed a class with sub properties in the inspector.
+	[Serializable]
+	public class Count
+	{
+		public int minimum; 			//Minimum value for our Count class.
+		public int maximum; 			//Maximum value for our Count class.
+		
+		
+		//Assignment constructor.
+		public Count (int min, int max)
+		{
+			minimum = min;
+			maximum = max;
+		}
+	}
+
+    public int columns = 5;
+    public int rows = 5;
+    public GameObject[] floorTiles;
+    private Transform boardHolder;
+    private Dictionary<Vector2, Vector2> gridPositions = new Dictionary<Vector2, Vector2>();
+
+    public void BoardSetup(){
+        boardHolder = new GameObject("Board").transform;
+
+        for (int x = 0; x < columns; x++){
+            for (int y = 0; y < rows; y++){
+                gridPositions.Add(new Vector2(x, y), new Vector2(x, y));
+
+                GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+                GameObject instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+    }
+
+}
